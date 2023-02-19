32ac85a849d535f05736e74fdc3296dc8724d215
YousefAllam221B <yousef.allam221b@gmail.com>
2023-02-20 01:38:11 +0200

    Change password and retype

--- dist/js/viewPasswordButton.js
+++ dist/js/viewPasswordButton.js
@@ -14,5 +14,21 @@ ShowPasswordToggle.onclick = function() {
             togglePasswordButton.setAttribute("aria-label", "Show password as plain text. " + "Warning: this will display your password on the screen.")
         }
     }
-}
-;
+};
+var ShowPasswordToggle = document.querySelector("#newpass");
+ShowPasswordToggle.onclick = function() {
+    document.querySelector("#newpass").classList.add("input-password");
+    document.getElementById("toggle-password-newpass").classList.remove("d-none");
+    const passwordInput = document.querySelector("#newpass");
+    const togglePasswordButton = document.getElementById("toggle-password-newpass");
+    togglePasswordButton.addEventListener("click", togglePassword);
+    function togglePassword() {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            togglePasswordButton.setAttribute("aria-label", "Hide password.")
+        } else {
+            passwordInput.type = "password";
+            togglePasswordButton.setAttribute("aria-label", "Show password as plain text. " + "Warning: this will display your password on the screen.")
+        }
+    }
+};
