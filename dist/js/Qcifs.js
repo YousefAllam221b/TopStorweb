75931f119dd723cc8be0a42f47bce1f02d744edc
YousefAllam221B <yousef.allam221b@gmail.com>
2023-02-19 23:58:55 +0200

    fixes

--- dist/js/Qcifs.js
+++ dist/js/Qcifs.js
@@ -54,32 +54,24 @@ var firstRequests = 7;
 //if (window.location.pathname.endsWith("Qnfs.html")) firstRequests = 2;
 //if (prot == 'NFS') firstRequests = 7;
 
-var ShowPasswordToggle = document.querySelector("#dompass");
-ShowPasswordToggle.onclick = function () {
-  document.querySelector("#dompass").classList.add("input-password");
-  document.getElementById("toggle-password").classList.remove("d-none");
-
-  const passwordInput = document.querySelector("#dompass");
-  const togglePasswordButton = document.getElementById("toggle-password");
-
-  togglePasswordButton.addEventListener("click", togglePassword);
-  function togglePassword() {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      togglePasswordButton.setAttribute("aria-label", "Hide password.");
-    } else {
-      passwordInput.type = "password";
-      togglePasswordButton.setAttribute(
-        "aria-label",
-        "Show password as plain text. " +
-          "Warning: this will display your password on the screen."
-      );
-	  
+var ShowPasswordToggle = document.querySelector("[type='password']");
+ShowPasswordToggle.onclick = function() {
+    document.querySelector("[type='password']").classList.add("input-password");
+    document.getElementById("toggle-password").classList.remove("d-none");
+    const passwordInput = document.querySelector("[type='password']");
+    const togglePasswordButton = document.getElementById("toggle-password");
+    togglePasswordButton.addEventListener("click", togglePassword);
+    function togglePassword() {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            togglePasswordButton.setAttribute("aria-label", "Hide password.")
+        } else {
+            passwordInput.type = "password";
+            togglePasswordButton.setAttribute("aria-label", "Show password as plain text. " + "Warning: this will display your password on the screen.")
+        }
     }
-  }
-};
-
-
+}
+;
 function poolsrefresh(first = 0) {
 	$(".select2.pool").select2({
 		placeholder: "Select a pool",
