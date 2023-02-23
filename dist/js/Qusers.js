0d3430e41edbafc03896795ca89c323f3874f689
YousefAllam221B <yousef.allam221b@gmail.com>
2023-02-23 20:29:11 +0200

    fixing

--- dist/js/Qusers.js
+++ dist/js/Qusers.js
@@ -66,72 +66,39 @@ $("#User").change(function (e) {
 	}
 });
 
-// let ExcelToJSONParser = function() {
-//     this.parseExcel = function(file) {
-//       var reader = new FileReader();
-//       reader.onload = function(e) {
-//         var data = e.target.result;
-//         var workbook = XLSX.read(data, {
-//           type: 'binary'
-//         });
-//         workbook.SheetNames.forEach(function(sheetName) {
-//           var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-//           var json_object = JSON.stringify(XL_row_object);
-//           console.log(JSON.parse(json_object));
-//           jQuery('#xlx_json').val(json_object);
-//         })
-//       };
-//       reader.onerror = function(ex) {
-//         console.log(ex);
-//       };
-//       reader.readAsBinaryString(file);
-//     };
-//   };
+let ExcelToJSONParser = function() {
+    this.parseExcel = function(file) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+          type: 'binary'
+        });
+        workbook.SheetNames.forEach(function(sheetName) {
+          var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+          var json_object = JSON.stringify(XL_row_object);
+          return console.log(JSON.parse(json_object));
+          jQuery('#xlx_json').val(json_object);
+        })
+      };
+      reader.onerror = function(ex) {
+        console.log(ex);
+      };
+      reader.readAsBinaryString(file);
+    };
+  };
 
-// function uploadFile(e) {
-//     var files = e.target.files; 
-//     var parsedExcel = new ExcelToJSONParser();
-// 	return parsedExcel.parseExcel(files[0]
-// }
-
-function importFile(e) {
-	var f = e.target.files[0];
-  
-	if (f) {
-	  var r = new FileReader();
-	  r.onload = e => {
-		var contents = processExcel(e.target.result);
-		console.log(contents)
-	  }
-	  r.readAsBinaryString(f);
-	} else {
-	  console.log("Failed to load file");
+function uploadFile(e) {
+    var files = e.target.files; 
+    var parsedExcel = new ExcelToJSONParser();
+	return parsedExcel.parseExcel(files[0]);
+}
+$('#uploaderInput').change(function(e) {
+	let users = uploadFile(e);
+	for (user in users){
+		console.log
 	}
-  }
-  
-function processExcel(data) {
-var workbook = XLSX.read(data, {
-	type: 'binary'
-});
-
-var firstSheet = workbook.SheetNames[0];
-var data = to_json(workbook);
-return data
-};
-
-function to_json(workbook) {
-var result = {};
-workbook.SheetNames.forEach(function(sheetName) {
-	var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
-	header: 1
-	});
-	if (roa.length) result[sheetName] = roa;
-});
-return JSON.stringify(result, 2, 2);
-};
-
-$('#uploaderInput').change(importFile);
-
+})
 $('#upload-file-btn').click(function() {
 	var form_data = new FormData($('#upload-file')[0]);
 	$.ajax({
